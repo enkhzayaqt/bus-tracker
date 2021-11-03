a3e7617dc194aa065cc5f7af9c85a04fb40d8b42
enkhzayaqt <jinizaya@gmail.com>
2021-11-03 18:38:07 -0500

    change file

--- mapanimation.js
+++ mapanimation.js
@@ -1,48 +1,37 @@
-// This array contains the coordinates for all bus stops between MIT and Harvard
-const busStops = [
-  [-71.093729, 42.359244],
-  [-71.094915, 42.360175],
-  [-71.0958, 42.360698],
-  [-71.099558, 42.362953],
-  [-71.103476, 42.365248],
-  [-71.106067, 42.366806],
-  [-71.108717, 42.368355],
-  [-71.110799, 42.369192],
-  [-71.113095, 42.370218],
-  [-71.115476, 42.372085],
-  [-71.117585, 42.373016],
-  [-71.118625, 42.374863],
-];
-
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZW1tYS04OSIsImEiOiJja3V5d24xbnE3OGJpMzJuemNidTdqMWR1In0.zssWjk0QXWARFP4L4bVuVg";
 
-// This is the map instance
-let map = new mapboxgl.Map({
+var map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v11",
-  center: [-71.104081, 42.365554],
-  zoom: 14,
+  center: [-88.03229326591754, 42.061823519463445],
+  zoom: 12,
 });
 
-// marker for first coordinates in the array busStops.
-const marker = new mapboxgl.Marker()
-  .setLngLat([-71.093729, 42.359244])
+var marker = new mapboxgl.Marker()
+  .setLngLat([-88.03229326591754, 42.061823519463445])
   .addTo(map);
-// counter here represents the index of the current bus stop
-let counter = 0;
+
+const stops = [
+  [-88.033054, 42.063646],
+  [-88.031058, 42.063136],
+  [-88.028547, 42.06261],
+  [-88.025736, 42.062005],
+  [-88.021359, 42.061193],
+  [-88.00302, 42.056856],
+  [-87.999576, 42.055406],
+  [-87.996282, 42.05304],
+  [-87.993761, 42.051343],
+  [-87.988547, 42.051343],
+  [-87.976352, 42.051338],
+];
+
+var counter = 0;
 function move() {
-  // marker moves on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
   setTimeout(() => {
-    if (counter >= busStops.length) return;
-    marker.setLngLat(busStops[counter]);
+    if (counter >= stops.length) return;
+    marker.setLngLat(stops[counter]);
     counter++;
     move();
   }, 1000);
-  // Using counter to access bus stops in the array busStops
-  // Make sure you call move() after you increment the counter.
-}
-
-if (typeof module !== "undefined") {
-  module.exports = { move };
 }
